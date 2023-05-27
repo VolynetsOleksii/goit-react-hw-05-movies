b78fba4d9c877e4f387c171a567296ba8e999b3d
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-05-27 23:45:27 +0300

    add App Layout and Header

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import Header from "components/Header/Header";
+import Loader from "components/Loader/Loader";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+
+
+const Layout = () => {
+    return (
+      <>
+        <Header />
+        <main>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </main>
+      </>
+    );
+  };
+  
+  export default Layout;
